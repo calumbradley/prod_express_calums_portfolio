9f55abf9530f75b3ddbd211e287ea1a8485f98a6
Calum Bradley <cal@gmx.co.uk>
2022-05-12 20:29:31 +0100

    a simple express server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node JS')
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`)
+})
