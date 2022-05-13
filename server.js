449fe87516de7ef2d0f154aaa42f49f7e582791e
Calum Bradley <cal@gmx.co.uk>
2022-05-13 11:59:37 +0100

    building a prod deployment

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express')
+const path = require('path');
+
 require('dotenv').config()
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello from Node JS')
-})
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const port = process.env.PORT || 5001
+
 
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`)
 })
+
+
+app.get('/api', function (req, res) {
+  res.json({1: "json"})
+});
