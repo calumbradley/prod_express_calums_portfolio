90d886172a20563f037358d7ae0f6e97e55f4cc4
Calum Bradley <cal@gmx.co.uk>
2022-05-13 10:42:15 +0100

    making a simple server path

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   res.send('Hello from Node JS')
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5001
 
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`)
