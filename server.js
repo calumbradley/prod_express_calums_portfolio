d9d30388191d6e44e192d620097f9d6f1f68e593
Calum Bradley <cal@gmx.co.uk>
2022-05-16 18:43:12 +0100

    getting json

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const express = require('express')
-const path = require('path');
+const path = require('path')
 
 require('dotenv').config()
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')))
+// Body Parser
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json()) // To parse the incoming requests with JSON payloads
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.post('/api', (req, res) => {
+  console.log(req.body)
+  res.sendStatus(200)
+})
 
 const port = process.env.PORT || 5001
-
 
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`)
 })
-
-
-app.get('/api', function (req, res) {
-  res.json({1: "json"})
-});
